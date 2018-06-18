974bf802f9876d43da428b92483aad1bf21d24fd
bshakir <burhanshakir96@gmail.com>
2018-06-18 12:52:11 +0100

    Initial commit

---
+++ src/main/java/io/github/burhanshakir/Blog.java
@@ -0,0 +1,66 @@
+package io.github.burhanshakir;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Blog")
+public class Blog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name="title")
+    private String title;
+
+    @Column(name="content")
+    private String content;
+
+    public Blog() { }
+
+    public Blog(int id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Blog(String title, String content) {
+        this.setTitle(title);
+        this.setContent(content);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString(){
+
+        return "Blog{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+
+    }
+}
---
+++ src/main/java/io/github/burhanshakir/BlogRepository.java
@@ -0,0 +1,12 @@
+package io.github.burhanshakir;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Blog, Integer>{
+
+    List<Blog> findByTitleContainingOrContentContaining(String text, String textAgain);
+}
